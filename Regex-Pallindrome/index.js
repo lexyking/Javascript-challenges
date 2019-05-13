2354bcb0b174bd29fffc917f5a6666eabff23693
lexyking <sahiealexandre@hotmail.fr>
2019-05-13 18:09:07 +0100

    the js of the project

--- Regex-Pallindrome/index.js
+++ Regex-Pallindrome/index.js
@@ -1,9 +1,33 @@
+const entry = document.querySelector('.inputString');
+const submitBtn = document.querySelector('.submitBtn');
+const exit = document.querySelector('.exit p')
+
+submitBtn.addEventListener('click', () => {
+    if (isValid(entry.value)) {
+        if (checkPallindrome(entry.value)) {
+            console.log('congratulations');
+            console.log(exit);
+            return exit.innerHTML = 'Congratulations';
+        } else {
+            alert(`Sorry '${entry.value}' is not a pallindrome. Try another word.`)
+        }
+    } else {
+        alert('Please Write a word...')
+    }
+
+
+})
+
+const isValid = (str) => {
+    return (str.length !== 0);
+}
+
 const checkPallindrome = inputStr => {
     let tempStr = inputStr.match(/[a-z0-9]+/ig).join('').toLowerCase();
 
     let second = tempStr.split('').reverse().join('');
 
-    console.log(tempStr === second);
+    return (tempStr === second);
 }
 
 const str = 'KAYAK'
